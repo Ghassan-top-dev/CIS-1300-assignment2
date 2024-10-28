eeaa128f23adccdf8acae7d77138a2829f5f1e24
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-10-27 20:12:22 -0400

    Create muradAghaGhassanA2.c
    This is where the function headers and bodies are

---
+++ muradAghaGhassanA2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "givenA2.h"
+
+
+int printer(int printy){
+
+    int outy = 300;
+    int nothing = 69;
+
+    if(printy == 1){
+
+        nothing = outy; 
+    }
+
+    return nothing;
+
+
+
+
+}
+
