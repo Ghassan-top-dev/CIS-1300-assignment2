95fc5f07f0f6b20cb5775e3b2e273bfc954c6e1c
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-10-27 20:12:59 -0400

    Create muradAghaGhassanMainA2.c
    This is where the function calls and main is

---
+++ muradAghaGhassanMainA2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "givenA2.h"
+
+
+
+int main(){
+
+    int yes_no = 0;
+    int result;
+
+    result = printer(yes_no);
+
+    printf("%d", result);
+
+
+
+
+
+    return 0; 
+}
