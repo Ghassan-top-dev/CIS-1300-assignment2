5919d5802f00be208e1abba3d5a26262572eb196
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-11-04 18:02:50 -0500

    Update formatStr.c

--- formatStr.c
+++ formatStr.c
@@ -1,5 +1,10 @@
-//how can I turn the smaller binary values into a long binary value
+//how can I turn the smaller binary values into a long binary value - calm down baby
+
+//This will turn string with dots into smaller blocks of binary integers (4 of them)
+//convert each block into a string, concatanate, then turn into long int using atol
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
     char ip[] = "198.78.1.0";  // IP address to process
@@ -8,9 +13,12 @@ int main() {
     int i, j, div;
     int bits = 0; //how many iterations? 8 of course
     long finalLong;
+    char placeHolderTemp[36] = ""; //this will temperorially hold each binary block
+    char binaryStr[36] = ""; //this will hold the full binary string
 
     // Attempt to parse IP address into four integers
     int result = sscanf(ip, "%d.%d.%d.%d", &octets[0], &octets[1], &octets[2], &octets[3]);
+
 
     // Check if all four octets were parsed successfully
     if (result == 4) {
@@ -42,10 +50,22 @@ int main() {
 
         for(j = bits -1; j>=0;j--){
 
-            printf("%d", newOctet[j]); 
+            sprintf(placeHolderTemp, "%d", newOctet[j]);
+            strcat(binaryStr,placeHolderTemp); 
+
+ 
         }
+        
+
+        
     }
+
+    printf("\n%s", binaryStr);
     
+    finalLong = strtol(binaryStr, NULL, 2);
+
+    printf("\n%ld", finalLong);
+
 
 
     
