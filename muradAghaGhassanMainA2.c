76e74028e9865c99f5a63b08c7eddef97f80a4a1
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-11-04 23:28:01 -0500

    Update muradAghaGhassanMainA2.c

--- muradAghaGhassanMainA2.c
+++ muradAghaGhassanMainA2.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "givenA2.h"
 
 
 int main(){
 
-    char ipAddress[16]; //is this legal? is this what I am suppoused to do?
+    char ipAddress[16]; //is this legal? is this what I am suppoused to do? - YES
 
     readIPAddress (ipAddress);
+    printf("IP Addr: %s", ipAddress); 
 
+
+    
 
 
 
