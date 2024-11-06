eccc8eb22143c637ff076505d5cbb10eba313afe
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-11-06 10:21:56 -0500

    Update muradAghaGhassanMainA2.c

--- muradAghaGhassanMainA2.c
+++ muradAghaGhassanMainA2.c
@@ -6,11 +6,20 @@
 
 int main(){
 
-    char ipAddress[16]; //is this legal? is this what I am suppoused to do? - YES
+    char ipAddress[36]; //is this legal? is this what I am suppoused to do? - YES
+
+    int *ptr = 0; 
 
     readIPAddress (ipAddress);
     printf("IP Addr: %s", ipAddress); 
 
+    // countDig(ipAddress); I don't understand how I am suppoused to get the length of ipAddress
+    //when it is a string and countDig takes an int
+    // printf("Length of ip address: %d", ipAddress); 
+
+    // convertIPToLongNumber(ipAddress, 0, ptr);
+
+    printf("\n\n%ld", convertIPToLongNumber(ipAddress, 0, ptr));
 
     
 
