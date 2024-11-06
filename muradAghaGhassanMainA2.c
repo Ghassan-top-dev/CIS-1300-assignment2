5f214669e4d3113d96f3f2b27220abed10b93660
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-11-06 12:12:38 -0500

    Update muradAghaGhassanMainA2.c

--- muradAghaGhassanMainA2.c
+++ muradAghaGhassanMainA2.c
@@ -6,25 +6,25 @@
 
 int main(){
 
-    char ipAddress[36]; //is this legal? is this what I am suppoused to do? - YES
+    char ipAddress[16]; //is this legal? is this what I am suppoused to do? - YES
 
     int *ptr = 0; 
+    printf("Testing Task 1 in main\n\n"); //do I have these here?
 
     readIPAddress (ipAddress);
-    printf("IP Addr: %s", ipAddress); 
+    printf("IP Addr: %s", ipAddress); //task 1
+    printf("\n\nTask 1 ends"); //do I have these here?
 
-    // countDig(ipAddress); I don't understand how I am suppoused to get the length of ipAddress
-    //when it is a string and countDig takes an int
-    // printf("Length of ip address: %d", ipAddress); 
 
-    // convertIPToLongNumber(ipAddress, 0, ptr);
-
-    printf("\n\n%ld", convertIPToLongNumber(ipAddress, 0, ptr));
-
+    long decimal = convertIPToLongNumber(ipAddress, 0, ptr); 
     
+    printf("\n\nTesting Task 2 in main"); //do I have these here?
+    printf("\n\nipDecimal = %ld", decimal); //task 2
+    printf("\nNumber of digits  = %d", countDig(decimal)); //task 2
+    printf("\nTask 2 ends"); //do I have these here?
 
-
-
-
+    printf("\n\nTesting Task 3 in main"); //do I have these here?
+    printf("\n\nClass for %s = %c", ipAddress, classifyIPAddress(ipAddress)); //task 3
+    printf("\nTask 3 ends\n\n"); //do I have these here?
     return 0; 
 }
