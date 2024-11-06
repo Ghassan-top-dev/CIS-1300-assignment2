3f24268d57c78d0572ce0424e3dfd5c685cb91e0
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-11-06 12:12:59 -0500

    Update muradAghaGhassanA2.c

--- muradAghaGhassanA2.c
+++ muradAghaGhassanA2.c
@@ -3,163 +3,42 @@
 #include <stdlib.h>
 #include "givenA2.h"
 
-//gcc -Wall -std=c99 muradAghaGhassanA2Main.c muradAghaGhassanA2.c
-
-
-// Task 1
-//is this how the function should be or do I need to have scanf in main? - this is fine
-void readIPAddress (char ipAddress []){
+void readIPAddress (char ipAddress []){ // Task 1
 
     int octetInput1 = -1, octetInput2 = -1, octetInput3 = -1, octetInput4 = -1; 
     ipAddress[0] = '\0';
     char string1[10], string2[10], string3[10], string4[10];
 
-    //octet#1
-    while (octetInput1 < 0 || octetInput1 > 127){
+    while (octetInput1 < 0 || octetInput1 > 127){     //octet#1
         printf("Octet#1 - Enter a number - must be between 0 and 127: ");
         scanf("%d", &octetInput1); 
     }
     sprintf(string1, "%d.", octetInput1); 
     strcat(ipAddress,string1);
 
-    //octet#2
-    while (octetInput2 < 0 || octetInput2 > 255){
+    while (octetInput2 < 0 || octetInput2 > 255){     //octet#2
         printf("Octet#2 - Enter a number - must be between 0 and 255: ");
         scanf("%d", &octetInput2); 
     }
     sprintf(string2, "%d.", octetInput2); 
     strcat(ipAddress, string2);
 
-    //octet#3
-    while (octetInput3 < 0 || octetInput3 > 255){
+    while (octetInput3 < 0 || octetInput3 > 255){    //octet#3
         printf("Octet#3 - Enter a number - must be between 0 and 255: ");
         scanf("%d", &octetInput3); 
     }
     sprintf(string3, "%d.", octetInput3); 
     strcat(ipAddress, string3);
 
-    //octet#4
-    while (octetInput4 < 0 || octetInput4 > 255){
+    while (octetInput4 < 0 || octetInput4 > 255){     //octet#4
         printf("Octet#4 - Enter a number - must be between 0 and 255: ");
         scanf("%d", &octetInput4); 
     }
     sprintf(string4, "%d", octetInput4); 
     strcat(ipAddress, string4);
-
-    //is this how I am suppoused to print the function?
-    //it says not to print it - print it in main
-
 }
 
-
-//task 2
-
-
-
-// int countDig (int); //do I have to put the prototype? or do I just put the header and body? // put prototype
-
-// int countDig (int){
-
-//     char ipAddress[16] = ""; 
-
-//     int ipLength = strlen(ipAddress) - 3;
-
-
-
-// } I asked a question in main...
-
-
-
-
-/*
-
-int charToInt(char ipAddress []); //prototype. can I have the prototype here? Do I even need it???
-
-int charToInt(char ipAddress []){
-    char newIp1[4]; 
-    char newIp2[4]; 
-    char newIp3[4]; 
-    char newIp4[4]; 
-
-    char *token = strtok(ipAddress, ".");
-
-    while (token != NULL) {
-
-        // Copy token into newIp1
-        for (int i = 0; i < 3 && token[i] != '\0'; i++) {
-            newIp1[i] = token[i];
-        }
-        newIp1[3] = '\0';  // Null-terminate newIp1 for safe string handling
-        
-        token = strtok(NULL, ".");
-        break;
-    }
-    
-    while (token != NULL) {
-
-        // Copy token into newIp1
-        for (int i = 0; i < 3 && token[i] != '\0'; i++) {
-            newIp2[i] = token[i];
-        }
-        newIp2[3] = '\0';  // Null-terminate newIp1 for safe string handling
-        
-        token = strtok(NULL, ".");
-        break;
-    }
-    
-    while (token != NULL) {
-
-        // Copy token into newIp1
-        for (int i = 0; i < 3 && token[i] != '\0'; i++) {
-            newIp3[i] = token[i];
-        }
-        newIp3[3] = '\0';  // Null-terminate newIp1 for safe string handling
-        
-        token = strtok(NULL, ".");
-        break;
-    }
-    
-    while (token != NULL) {
-
-        // Copy token into newIp1
-        for (int i = 0; i < 3 && token[i] != '\0'; i++) {
-            newIp4[i] = token[i];
-        }
-        newIp4[3] = '\0';  // Null-terminate newIp1 for safe string handling
-        
-        token = strtok(NULL, ".");
-        break;
-    }
-    
-    // Print newIp1 as a string
-    printf("%s\n", newIp1);
-    printf("%s\n", newIp2);
-    printf("%s\n", newIp3);
-    printf("%s\n", newIp4);
-
-
-
-
-}
-
-
-*/
-
-
-
-
-/*
-
-
-int countDig (char ipAddress[]){
-
-        int lengthIPAddr = strlen(ipAddress)
-    }
-
-*/    
-
-//task1      might need    
-long int convertIPToLongNumber ( char ipAddress [], int lengthIPAddr, int * numDigits){
+long int convertIPToLongNumber ( char ipAddress [], int lengthIPAddr, int * numDigits){ //task2       
     
     int octets[4];
     int i, j, div;
@@ -167,41 +46,64 @@ long int convertIPToLongNumber ( char ipAddress [], int lengthIPAddr, int * numD
     int newOctet[16]; 
     char placeHolderTemp[36] = ""; //this will temperorially hold each binary block
     char binaryStr[36] = ""; //this will hold the full binary string
+    long finalLong; 
 
-
-
-    int result = sscanf(ipAddress, "%d.%d.%d.%d", &octets[0], &octets[1], &octets[2], &octets[3]);
+    sscanf(ipAddress, "%d.%d.%d.%d", &octets[0], &octets[1], &octets[2], &octets[3]);
 
     for (i = 0; i < 4; i++) //run for all 4 octets
     {
         bits = 0;
         div = octets[i]; 
         do{
-            
             newOctet[bits] = div % 2;
             div = div / 2;
             bits++;
-            
-
         }while(bits != 8);
 
-
         for(j = bits -1; j>=0;j--){
-
             sprintf(placeHolderTemp, "%d", newOctet[j]);
             strcat(binaryStr,placeHolderTemp); 
         }
-        
     }
 
-
-    long finalLong; 
-
     finalLong = strtol(binaryStr, NULL, 2);
-
     return finalLong; 
-    
-
-
-
 }
+
+int countDig (int finalLong){ //digit counter
+
+    int counter = 0; 
+    do {
+    finalLong /= 10;
+    ++counter;
+    } while (finalLong != 0);
+
+    return counter; 
+}
+
+
+char classifyIPAddress (char ipAddress []){ //task 3
+
+    int octets[4];
+
+    sscanf(ipAddress, "%d.%d.%d.%d", &octets[0], &octets[1], &octets[2], &octets[3]);
+
+    if (octets[1] >= 240 && octets[1] <= 255) {
+        return 'E'; 
+    } 
+    else if (octets[1] >= 224 && octets[1] <= 239) {
+        return 'D'; 
+    } 
+    else if (octets[1] >= 192 && octets[1] <= 223) {
+        return 'C'; 
+    } 
+    else if (octets[1] >= 128 && octets[1] <= 191) {
+        return 'B'; 
+    } 
+    else if (octets[1] >= 0 && octets[1] <= 127) {
+        return 'A'; 
+    }else{
+        return 0; 
+    }
+}
+
