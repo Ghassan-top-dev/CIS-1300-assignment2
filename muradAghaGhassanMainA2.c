513e495b4ae87f57b4083373a5114f126e59f8e5
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-11-07 17:48:51 -0500

    Update muradAghaGhassanMainA2.c

--- muradAghaGhassanMainA2.c
+++ muradAghaGhassanMainA2.c
@@ -4,27 +4,35 @@
 #include "givenA2.h"
 
 
+
 int main(){
+   
 
-    char ipAddress[16]; //is this legal? is this what I am suppoused to do? - YES
 
-    int *ptr = 0; 
-    printf("Testing Task 1 in main\n\n"); //do I have these here?
+    char ipAddress[16]; 
+    int numDigits = 0;
+    int lengthIPAddr = 300; 
+
+    printf("Testing Task 1 in main\n\n"); 
 
     readIPAddress (ipAddress);
     printf("IP Addr: %s", ipAddress); //task 1
-    printf("\n\nTask 1 ends"); //do I have these here?
+    printf("\n\nTask 1 ends"); 
 
 
-    long decimal = convertIPToLongNumber(ipAddress, 0, ptr); 
+    long decimal = convertIPToLongNumber(ipAddress, lengthIPAddr, &numDigits); 
     
-    printf("\n\nTesting Task 2 in main"); //do I have these here?
+    printf("\n\nTesting Task 2 in main"); 
     printf("\n\nipDecimal = %ld", decimal); //task 2
-    printf("\nNumber of digits  = %d", countDig(decimal)); //task 2
-    printf("\nTask 2 ends"); //do I have these here?
+    printf("\nNumber of digits  = %d", numDigits); //task 2
+    printf("\nTask 2 ends"); 
 
-    printf("\n\nTesting Task 3 in main"); //do I have these here?
+    printf("\n\nTesting Task 3 in main"); 
     printf("\n\nClass for %s = %c", ipAddress, classifyIPAddress(ipAddress)); //task 3
-    printf("\nTask 3 ends\n\n"); //do I have these here?
+    printf("\nTask 3 ends\n\n"); 
+
+
+
+    
     return 0; 
 }
